5aa1485b61ef1988cb079dd239b639fcc02bd2ad
Soumyadeep <soumyadeep9@gmail.com>
2015-06-05 09:42:26 +0200

    Prof List updated

---
+++ web/hazardous.php
@@ -0,0 +1,28 @@
+<?php
+ include 'header.php';
+?>
+
+<section class="callaction">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="big-cta">
+					<div class="cta-text">
+						<h2>Robotic Framework for exploring rugged and potentially hazardous terrain </h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
+	<br/>
+<div style="width:80%;margin-left:10%;font-size:18px; text-align: justify;">
+	This project has got a potential benefits in exploration of a rugged terrain where Human  fails in maneuvering due to the instability of their body dynamics as well as in reaching  hazardous places, which are immensely dangerous for a human being to move through. This project is to develop a robotic non-wheeled framework having the capability to go through the utmost rugged terrains as well as to exercise immense stability. The biped being a highly unstable system a quadruped is been assumed to have more stability and thus, will be used for this purpose. These bots should have in them in-built push recovery modules so as to balance themselves from any sudden thrust. 
+<br/>
+<br/> 	 	
+</div>
+<br/>
+<br/>
+<?php
+ include 'footer.php';
+?>
